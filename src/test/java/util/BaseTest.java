c8ad82d700f112a215afab6b67699514cc0c78ea
pooja <poojagtap8592@gmail.com>
2023-09-08 04:21:34 -0400

    commit by pooja

---
+++ src/test/java/util/BaseTest.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static org.hamcrest.Matchers.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.jsonpath.JsonPath;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.restassured.RestAssured;
+
+public class BaseTest {
+	
+	private static final Logger logger = LogManager.getLogger(BaseTest.class);
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		//RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+	
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+		if(result.getStatus() == ITestResult.FAILURE) {
+			Throwable t = result.getThrowable();
+			StringWriter error = new StringWriter();
+			t.printStackTrace(new PrintWriter(error));
+			logger.info(error.toString());
+		}
+		
+	}
+	
+}
