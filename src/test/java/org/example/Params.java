c419c0766b223e2301188877fb88b3c232ed7fc5
pooja <poojagtap8592@gmail.com>
2023-09-10 12:58:19 -0400

    commit by pooja

---
+++ src/test/java/org/example/Params.java
@@ -0,0 +1,20 @@
+package org.example;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+public class Params {
+
+    @Test
+    @Parameters ({"val1", "val2"})
+    public void Sum(int v1, int v2) {
+        int finalsum = v1 + v2;
+        System.out.println("The final sum of the given values is " + finalsum);
+    }
+
+
+    @Test
+    @Parameters ({"val1", "val2"})
+    public void Diff (int v1, int v2) {
+        int finaldiff = v1 - v2;
+        System.out.println("The final difference of the given values is " + finaldiff);
+    }
+}
